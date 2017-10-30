88929afcfef21d0a07beafbd26d26d4ed4899f4b
kamgamboa <kmllgmb@gmail.com>
2017-10-30 14:21:52 +0800

    Added mobility and programs table in app programs

--- programs/models.py
+++ programs/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+from django.db.models import ForeignKey, CharField, DateField
+
+from institutions.models import Institution
+
+
+class Mobility:
+    linkage = ForeignKey(Institution)
+    name = CharField(max_length=64)
+
+
+class Program:
+    mobility = ForeignKey(Mobility)
+    start_date = DateField()
+    end_date = DateField()
