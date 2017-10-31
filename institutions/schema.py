249a865186ddbf3f4efeed384dbd4f2b6366b3e1
Keith Tan <skeithtan@gmail.com>
2017-10-31 18:29:47 +0800

    Added single institution GraphQL View

--- institutions/schema.py
+++ institutions/schema.py
@@ -1,11 +1,18 @@
-from graphene import AbstractType, List
 from graphene_django.types import DjangoObjectType
 from .models import *
+
+from graphene import (
+    AbstractType,
+    List,
+    Field,
+    Int
+)
 
 
 class CountryType(DjangoObjectType):
     class Meta:
         model = Country
+
 
 class InstitutionType(DjangoObjectType):
     class Meta:
@@ -16,8 +23,14 @@ class Query(AbstractType):
     countries = List(CountryType)
     institutions = List(InstitutionType)
 
+    institution = Field(InstitutionType, id=Int())
+
     def resolve_countries(self, info, **kwargs):
         return [country for country in Country.objects.all() if country.institution_set.count() > 0]
 
     def resolve_institutions(self, info, **kwargs):
         return Institution.objects.all()
+
+    def resolve_institution(self, info, **kwargs):
+        id = kwargs.get('id')
+        return Institution.objects.get(pk=id)
