e11d9b2f3a18ea4d7be43fb71df4bebe5f8274e5
Keith Tan <skeithtan@gmail.com>
2017-11-02 07:45:37 +0800

    Fixed urls.py again

--- institutions/urls.py
+++ institutions/urls.py
@@ -3,17 +3,17 @@ from .views import *
 
 institution_urls = [
     url(r'institutions/$', InstitutionListCreateView.as_view()),
-    url(r'institutions/^(?P<pk>(\d+))/$', InstitutionUpdateDestroyRetrieveView.as_view()),
-    url(r'institutions/^(?P<institution_id>(\d+))/memorandums/$', MemorandumListCreateView.as_view()),
-    url(r'institutions/^(?P<institution_id>(\d+))/programs/$', ProgramListCreateView.as_view()),
+    url(r'institutions/(?P<pk>(\d+))/$', InstitutionUpdateDestroyRetrieveView.as_view()),
+    url(r'institutions/(?P<institution_id>(\d+))/memorandums/$', MemorandumListCreateView.as_view()),
+    url(r'institutions/(?P<institution_id>(\d+))/programs/$', ProgramListCreateView.as_view()),
 ]
 
 memorandum_urls = [
-    url(r'memorandums/^(?P<pk>(\d+))/$', MemorandumUpdateDestroyRetrieveView.as_view()),
+    url(r'memorandums/(?P<pk>(\d+))/$', MemorandumUpdateDestroyRetrieveView.as_view()),
 ]
 
 program_urls = [
-    url(r'programs/^(?P<pk>(\d+))/$', ProgramUpdateDestroyRetrieveView.as_view()),
-    url(r'programs/^(?P<program_id>(\d+))/offerings/^(?P<pk>(\d+))/$', ProgramOfferingListCreateView.as_view()),
-    url(r'program-offerings/^(?P<pk>(\d+))/$', ProgramOfferingRetrieveUpdateDestroyView.as_view()),
+    url(r'programs/(?P<pk>(\d+))/$', ProgramUpdateDestroyRetrieveView.as_view()),
+    url(r'programs/(?P<program_id>(\d+))/offerings/^(?P<pk>(\d+))/$', ProgramOfferingListCreateView.as_view()),
+    url(r'program-offerings/(?P<pk>(\d+))/$', ProgramOfferingRetrieveUpdateDestroyView.as_view()),
 ]
