a908e87918167382df8c1fe55b34762fdc58bf2e
Keith Tan <skeithtan@gmail.com>
2017-11-02 16:18:43 +0800

    Revert "Revert "Renamed gender to sex""
    This reverts commit b0238b79ad90db0ba87fc82040abf0145b8acd5f.

--- institutions/schema.py
+++ institutions/schema.py
@@ -19,11 +19,32 @@ class InstitutionType(DjangoObjectType):
         model = Institution
 
 
+class MemorandumType(DjangoObjectType):
+    class Meta:
+        model = Memorandum
+
+
+class ProgramType(DjangoObjectType):
+    class Meta:
+        model = Program
+
+
+class ProgramOfferingType(DjangoObjectType):
+    class Meta:
+        model = ProgramOffering
+
+
 class Query(ObjectType):
     countries = List(CountryType)
     institutions = List(InstitutionType)
+    memorandums = List(MemorandumType)
+    programs = List(ProgramType)
+    program_offerings = List(ProgramOfferingType)
 
     institution = Field(InstitutionType, id=Int())
+    memorandum = Field(MemorandumType, id=Int())
+    program = Field(ProgramType, id=Int())
+    program_offering = Field(ProgramOfferingType, id=Int())
 
     def resolve_countries(self, info, **kwargs):
         return [country for country in Country.objects.all() if country.institution_set.count() > 0]
