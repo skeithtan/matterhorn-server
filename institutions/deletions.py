ce363b29e37513ab0f4d931e5070ff0e147956c9
kamgamboa <kmllgmb@gmail.com>
2017-11-09 02:59:02 +0800

    Added RetrieveUpdateDestroyAPIViews in institution deletions.py

--- institutions/deletions.py
+++ institutions/deletions.py
@@ -1,4 +1,4 @@
-from rest_framework.generics import ListCreateAPIView
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from rest_framework.permissions import IsAuthenticated
 from institutions.serializers import *
 from institutions.models import *
@@ -10,13 +10,43 @@ class InstitutionBinView(ListCreateAPIView):
     serializer_class = InstitutionSerializer
 
 
+class InstitutionUpdateBinView(RetrieveUpdateDestroyAPIView):
+    permission_classes = (IsAuthenticated,)
+    queryset = Institution.all_objects.exclude(deleted_at=None)
+    serializer_class = InstitutionSerializer
+
+    def get_serializer(self, *args, **kwargs):
+        kwargs['partial'] = True
+        return super(InstitutionUpdateBinView, self).get_serializer(*args, **kwargs)
+
+
 class MemorandumBinView(ListCreateAPIView):
     permission_classes = (IsAuthenticated,)
     queryset = Memorandum.all_objects.exclude(deleted_at=None)
     serializer_class = MemorandumSerializer
 
 
+class MemorandumUpdateBinView(RetrieveUpdateDestroyAPIView):
+    permission_classes = (IsAuthenticated,)
+    queryset = Memorandum.all_objects.exclude(deleted_at=None)
+    serializer_class = MemorandumSerializer
+
+    def get_serializer(self, *args, **kwargs):
+        kwargs['partial'] = True
+        return super(MemorandumUpdateBinView, self).get_serializer(*args, **kwargs)
+
+
 class ProgramBinView(ListCreateAPIView):
     permission_classes = (IsAuthenticated,)
     queryset = Program.all_objects.exclude(deleted_at=None)
     serializer_class = ProgramSerializer
+
+
+class ProgramUpdateBinView(RetrieveUpdateDestroyAPIView):
+    permission_classes = (IsAuthenticated,)
+    queryset = Program.all_objects.exclude(deleted_at=None)
+    serializer_class = ProgramSerializer
+
+    def get_serializer(self, *args, **kwargs):
+        kwargs['partial'] = True
+        return super(ProgramUpdateBinView, self).get_serializer(*args, **kwargs)
