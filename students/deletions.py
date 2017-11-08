24ceb2aa8edec6b15eefed92c4a7ec0d1599a76a
kamgamboa <kmllgmb@gmail.com>
2017-11-09 03:04:51 +0800

    Added RetrieveUpdateDestroyAPIViews in students deletions.py

--- students/deletions.py
+++ students/deletions.py
@@ -1,4 +1,4 @@
-from rest_framework.generics import ListCreateAPIView
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from rest_framework.permissions import IsAuthenticated
 from students.serializers import *
 from students.models import *
@@ -10,13 +10,44 @@ class StudentBinView(ListCreateAPIView):
     serializer_class = StudentSerializer
 
 
+class StudentUpdateBinView(RetrieveUpdateDestroyAPIView):
+    permission_classes = (IsAuthenticated,)
+    queryset = Student.all_objects.exclude(deleted_at=None)
+    serializer_class = StudentSerializer
+
+    def get_serializer(self, *args, **kwargs):
+        kwargs['partial'] = True
+        return super(StudentUpdateBinView, self).get_serializer(*args, **kwargs)
+
+
 class ResidencyAddressHistoryBinView(ListCreateAPIView):
     permission_classes = (IsAuthenticated,)
     queryset = ResidencyAddressHistory.all_objects.exclude(deleted_at=None)
     serializer_class = ResidencyAddressHistorySerializer
 
 
+class ResidencyAddressHistoryUpdateBinView(RetrieveUpdateDestroyAPIView):
+    permission_classes = (IsAuthenticated,)
+    queryset = ResidencyAddressHistory.all_objects.exclude(deleted_at=None)
+    serializer_class = ResidencyAddressHistorySerializer
+
+    def get_serializer(self, *args, **kwargs):
+        kwargs['partial'] = True
+        return super(ResidencyAddressHistoryUpdateBinView, self).get_serializer(*args, **kwargs)
+
+
 class StudentProgramBinView(ListCreateAPIView):
     permission_classes = (IsAuthenticated,)
     queryset = StudentProgram.all_objects.exclude(deleted_at=None)
     serializer_class = StudentProgramSerializer
+
+
+class StudentProgramUpdateBinView(RetrieveUpdateDestroyAPIView):
+    permission_classes = (IsAuthenticated,)
+    queryset = StudentProgram.all_objects.exclude(deleted_at=None)
+    serializer_class = StudentProgramSerializer
+
+    def get_serializer(self, *args, **kwargs):
+        kwargs['partial'] = True
+        return super(StudentProgramUpdateBinView, self).get_serializer(*args, **kwargs)
+
--- students/urls.py
+++ students/urls.py
@@ -7,16 +7,22 @@ student_urls = [
     url(r'students/$', StudentListCreateView.as_view()),
     url(r'students/(?P<pk>(\d+))/$', StudentRetrieveUpdateDestroyView.as_view()),
     url(r'students/bin/$', StudentBinView.as_view()),
+    url(r'students/bin/(?P<pk>(\d+))/$', StudentUpdateBinView.as_view()),
+
 ]
 
 residency_urls = [
     url(r'students/(?P<student_id>(\d+))/residency/$', ResidencyAddressHistoryListCreateView.as_view()),
-    url(r'students/(?P<student_id>(\d+))/residency/(?P<residencyaddresshistory_id>(\d+))/$', ResidencyAddressHistoryRetrieveUpdateDestroyView.as_view()),
+    url(r'students/(?P<student_id>(\d+))/residency/(?P<pk>(\d+))/$', ResidencyAddressHistoryRetrieveUpdateDestroyView.as_view()),
     url(r'residency/bin/$', ResidencyAddressHistoryBinView.as_view()),
+    url(r'residency/bin/(?P<pk>(\d+))/$', ResidencyAddressHistoryUpdateBinView.as_view()),
+
 ]
 
 studentprogram_urls = [
     url(r'students/(?P<student_id>(\d+))/programs/$', StudentProgramListCreateView.as_view()),
     url(r'students/(?P<student_id>(\d+))/programs/(?P<program_offering_id>(\d+))/$', StudentProgramRetrieveUpdateDestroyView.as_view()),
     url(r'studentprograms/bin/$', StudentProgramBinView.as_view()),
+    url(r'studentprograms/bin/(?P<pk>(\d+))/$', StudentProgramUpdateBinView.as_view()),
+
 ]
