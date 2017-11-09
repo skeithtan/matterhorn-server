a51a4d057755bbb57def4df6916c2b512da10094
kamgamboa <kmllgmb@gmail.com>
2017-11-09 15:58:16 +0800

    Fixed institution urls.py

--- institutions/urls.py
+++ institutions/urls.py
@@ -7,7 +7,7 @@ institution_urls = [
     url(r'institutions/(?P<pk>(\d+))/$', InstitutionUpdateDestroyRetrieveView.as_view()),
     url(r'institutions/(?P<institution_id>(\d+))/memorandums/$', MemorandumListCreateView.as_view()),
     url(r'institutions/(?P<institution_id>(\d+))/programs/$', ProgramListCreateView.as_view()),
-    url(r'deleted/institutions$', DeletedInstitutionsView.as_view()),
+    url(r'deleted/institutions/$', DeletedInstitutionsView.as_view()),
     url(r'deleted/institutions/(?P<pk>(\d+))/$', DeletedInstitutionUpdateDeletedView.as_view()),
 
 ]
@@ -15,7 +15,7 @@ institution_urls = [
 memorandum_urls = [
     url(r'memorandums/(?P<pk>(\d+))/$', MemorandumUpdateDestroyRetrieveView.as_view()),
     url(r'deleted/memorandums/$', DeletedMemorandumsView.as_view()),
-    url(r'deleted/memorandums/(?P<pk>(\d+))$', DeletedMemorandumUpdateDeletedView.as_view()),
+    url(r'deleted/memorandums/(?P<pk>(\d+))/$', DeletedMemorandumUpdateDeletedView.as_view()),
 ]
 
 program_urls = [
