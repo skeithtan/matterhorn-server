8622416fcfeb2c8b9297cfb76fe782ad7bda3317
rjdeniega <ronald_deniega@dlsu.edu.ph>
2017-12-03 00:43:53 +0800

    Finalized Unit Reports [complete]
    pls report any bugs

--- core/views.py
+++ core/views.py
@@ -84,29 +84,82 @@ class UnitReportView(APIView):
 
         # Outbound programs with AY and term
         outbound_programs = [outbound_program for outbound_program
-                            in OutboundProgram.objects.all()
-                            if outbound_program.program.academic_year == academic_year
-                            and term in outbound_program.program.terms_available.all()]
+                             in OutboundProgram.objects.all()
+                             if outbound_program.program.academic_year == academic_year
+                             and term in outbound_program.program.terms_available.all()]
 
         inbound_programs = [inbound_program for inbound_program
-                             in InboundProgram.objects.all()
-                             if inbound_program.program.academic_year == academic_year
-                             and term in inbound_program.program.terms_available.all()]
+                            in InboundProgram.objects.all()
+                            if inbound_program.program.academic_year == academic_year
+                            and term in inbound_program.program.terms_available.all()]
 
         # get all deployed programs with specific AY and Term
         deployed_outbounds = [program for program
-                             in DeployedStudentProgram.objects.all()
-                             if program.student_program.program in outbound_programs]
+                              in DeployedStudentProgram.objects.all()
+                              if program.student_program.program in outbound_programs]
 
-        # summarize per institution
+        accepted_inbounds = [program for program
+                             in AcceptedStudentProgram.objects.all()
+                             if program.student_program.program in inbound_programs]
+
+        # summarize per institution and append outbound units
+        report_item = ReportItem()
+        report_items = []
         for institution in Institution.objects.all():
-            units_sum = 0
             for item in deployed_outbounds:
                 program_institution = item.student_program.program.institution
                 if program_institution == institution:
-                    units_sum += item.total_units_enrolled  # add all units of programs in institution
-                    data.append({
-                        "institution": institution.name,
-                        "outbound_units_enrolled": units_sum
-                    })
+                    report_item.institution = institution.name
+                    report_item.outbound_units_enrolled += item.total_units_enrolled
+
+                    existing_report = ReportItem.exist(institution.name, report_items)
+                    if not existing_report:
+                        report_items.append(report_item)
+                    else:
+                        existing_report.outbound_units_enrolled = \
+                            report_item.outbound_units_enrolled
+                else:
+                    break
+
+        # append inbound units
+        for inbound in accepted_inbounds:
+            for item in report_items:
+
+                if str(inbound.student_program.student.institution) == item.institution:
+                    existing_report = ReportItem.exist(item.institution, report_items)
+                    print(inbound.total_units_enrolled)
+                    report_item.inbound_units_enrolled += inbound.total_units_enrolled
+
+                    if not existing_report:
+                        print("entered not existing")
+                        report_items.append(report_item)
+                    else:
+                        print("entered existing")
+                        existing_report.inbound_units_enrolled = report_item.inbound_units_enrolled
+
+        for item in report_items:
+            data.append({
+                "institution": item.institution,
+                "outbound_units_enrolled": item.outbound_units_enrolled,
+                "inbound_units_enrolled": item.inbound_units_enrolled
+            })
+
         return Response(data=data, status=200)
+
+
+class ReportItem(object):
+    institution = None
+    outbound_units_enrolled = 0
+    inbound_units_enrolled = 0
+
+    def __init__(self, inbound_units_enrolled=0, outbound_units_enrolled=0, institution=None):
+        self.institution_name = institution
+        self.outbound_units_enrolled = outbound_units_enrolled
+        self.inbound_units_enrolled = inbound_units_enrolled
+
+    @staticmethod
+    def exist(institution, list):
+        for item in list:
+            if item.institution == institution:
+                return item
+        return False
--- institutions/admin.py
+++ institutions/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from students.models import DeployedStudentProgram, OutboundStudentProgram
+from students.models import DeployedStudentProgram, OutboundStudentProgram, AcceptedStudentProgram, \
+    InboundStudentProgram
 from .models import *
 
 admin.site.register(Institution)
@@ -13,4 +14,6 @@ admin.site.register(InboundProgram)
 admin.site.register(OutboundProgram)
 admin.site.register(OutboundRequirement)
 admin.site.register(DeployedStudentProgram)
+admin.site.register(AcceptedStudentProgram)
+admin.site.register(InboundStudentProgram)
 
