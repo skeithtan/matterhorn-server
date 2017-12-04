3ce2cde54e4be53f2bffb27564d7b3f264fb030b
rjdeniega <ronald_deniega@dlsu.edu.ph>
2017-12-04 20:30:38 +0800

    removed Outbound and Inbound Requirements program foreign key

--- core/views.py
+++ core/views.py
@@ -228,9 +228,7 @@ class OutboundUnitsReportView(APIView):
                     else:
                         item = [item for item in data if item["institution"] == institution.name][0]
                         item["default_units"] += program.default_units
-                        print(item["total_units"])
                         item["total_units"] += program.total_units_enrolled
-                        print(f"{institution.name} and {item['total_units']}")
                         item["students"] += 1
 
         return Response(data=data, status=200)
